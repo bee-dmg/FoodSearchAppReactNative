e78800d98064b1dd51df4b1be0efc0c8d5a6523a
bee-dmg <brandonschmersal@gmail.com>
2022-09-08 09:46:52 -0400

    Working on Search.

--- App.js
+++ App.js
@@ -1,56 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TouchableWithoutFeedback, Animated, Easing } from 'react-native';
-import React, {useState} from 'react';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./.expo/src/screens/SearchScreen";
+const navigator = createStackNavigator({
+Search: SearchScreen,
+  },{
+    intitialRouteName: 'Search',
+    defaultNavigationOptions: { title:'Business Search'}
+  });
 
-export default function App() {
-const [boop, setBoop] = useState(false);
-let scaleValue = new Animated.Value(0)
-  const papaj = require('./assets/IMG_2531.jpg');
-  return (
-    <View style={styles.container}>
-      <Text>Pizza Pie</Text>
-      <Button title="Boop" onPress={()=>setBoop(!boop)}/>
-
-
-      <TouchableWithoutFeedback
-      onPressIn={() => {
-        scaleValue.setValue(0);
-        Animated.timing(scaleValue, {
-          toValue: 1,
-          duration: 250,
-          easing: Easing.linear,
-          useNativeDriver: true
-        }).start();
-
-        // cardAction();
-      }}
-      onPressOut={() => {
-        // Animated.timing(scaleValue, {
-        //   toValue: 0,
-        //   duration: 100,
-        //   easing: Easing.linear,
-        //   useNativeDriver: true
-        // }).start();
-        console.log("test")
-      }}
-    ><Image style={{height:200, width:200}} source={papaj} /></TouchableWithoutFeedback>
-      <TouchableOpacity onPress={()=>setBoop(!boop)}>
-      {!boop ? <Image style={{height:200, width:200}} source={papaj} /> : <Animated.Text style={styles.pastajohn}>Hello I'm Pasta Jawn</Animated.Text>}
-      
-      </TouchableOpacity><StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  pastajohn:{
-
-  },
-  
-});
+  export default createAppContainer(navigator);
