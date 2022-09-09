8ed7aa52a73fe385d6a072804f831de26b4cd183
Bee <69877469+bee-dmg@users.noreply.github.com>
2022-09-09 09:02:11 -0400

    Adding Yelp API.

---
+++ src/api/Yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://api.yelp.com/v3/businesses',
+    headers:{
+        Authorization:'Bearer s3fofCHkpIkEdrZeVECRdSsvfsiULPwvXgqipRZ5faMHhuxBn4CRtSvvdGKlCIdPlsxKXw_8xwo4OmO4z5m8-Vox1YCCYUh0wfToNKgMKSCH1CTrUBI9ix45d54YY3Yx',
+    }
+
+});
