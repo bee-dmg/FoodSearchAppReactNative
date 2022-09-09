fa09dae653eba890a65b316fe8a769e5c896fdf8
Bee <69877469+bee-dmg@users.noreply.github.com>
2022-09-09 08:38:46 -0400

    Updating file routes.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import SearchScreen from "./.expo/src/screens/SearchScreen";
-const navigator = createStackNavigator({
-Search: SearchScreen,
-  },{
-    intitialRouteName: 'Search',
-    defaultNavigationOptions: { title:'Business Search'}
-  });
+import SearchScreen from "./src/screens/SearchScreen";
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    intitialRouteName: "Search",
+    defaultNavigationOptions: { title: "Business Search" },
+  }
+);
 
-  export default createAppContainer(navigator);
+export default createAppContainer(navigator);
