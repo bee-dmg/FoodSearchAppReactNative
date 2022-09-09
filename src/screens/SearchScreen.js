a8d9d1d2ca81e8727d87ff74c75a63429c437d58
Bee <69877469+bee-dmg@users.noreply.github.com>
2022-09-09 09:23:13 -0400

    Updates.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+// import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-
+  const [results, setResults] = useState([]);
+  const apiKey = process.env.REACT_APP_YELP_KEY;
   return (
     <View>
       <SearchBar
@@ -12,6 +14,8 @@ const SearchScreen = () => {
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmit={() => console.log("term submited")}
       />
+      <Text>{apiKey}</Text>
+      {console.log(process.env)}
       <Text>Search Screen {term}</Text>
     </View>
   );
