0a4a7134b72c87518de0bc2d675e3d30c2ac40b6
Bee <69877469+bee-dmg@users.noreply.github.com>
2022-09-11 08:30:47 -0400

    Yelp API Search.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Yelp from "../api/Yelp";
 import SearchBar from "../components/SearchBar";
-// import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
-  const apiKey = process.env.REACT_APP_YELP_KEY;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const yelp = Yelp;
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: { limit: 50, term, location: 'san jose' },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term submited")}
-      />
-      <Text>{apiKey}</Text>
-      {console.log(process.env)}
-      <Text>Search Screen {term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ?<Text>{errorMessage}</Text>:null}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
